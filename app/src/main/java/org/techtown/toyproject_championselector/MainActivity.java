de5496f5f475a4aa6287f193daa8157404e0c904
LeeJH3904 <dasom8899@naver.com>
2022-11-21 23:04:37 +0900

    11/21 mainactivity.xml 완성 button_이름  = 이름 값의 사람

--- app/src/main/java/org/techtown/toyproject_championselector/MainActivity.java
+++ app/src/main/java/org/techtown/toyproject_championselector/MainActivity.java
@@ -3,12 +3,80 @@ package org.techtown.toyproject_championselector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+Button button_kih, button_ktw, button_nouh, button_pyh,button_ljh,button_ljs , button_yyj, button_uhj,button_jhk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //김인항
+        button_kih.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //김태원
+        button_ktw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //노은흠
+        button_nouh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //박영현
+        button_pyh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //이제현
+        button_ljh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //이준석
+        button_ljs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //윤영진
+        button_yyj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //엄희준
+        button_uhj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //조현기
+        button_jhk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/org/techtown/toyproject_championselector/MainActivity_kih.java
@@ -0,0 +1,29 @@
+package org.techtown.toyproject_championselector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity_kih extends AppCompatActivity {
+    private ListView listView = null;
+    private ListViewAdapter  adapter = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_kih);
+
+
+    }
+}
