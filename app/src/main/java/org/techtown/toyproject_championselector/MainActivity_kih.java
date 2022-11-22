361fcf9799d1a60df40802a0cbc3fcaf8c671c22
youngjin yoon <yyj1453@gmail.com>
2022-11-23 01:31:54 +0900

    11/23 MainActivity_kih / 106~132 line / get gallery_image

--- app/src/main/java/org/techtown/toyproject_championselector/MainActivity_kih.java
+++ app/src/main/java/org/techtown/toyproject_championselector/MainActivity_kih.java
@@ -1,29 +1,29 @@
 package org.techtown.toyproject_championselector;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import static android.os.SystemClock.sleep;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
-import android.media.SoundPool;
+import android.net.Uri;
 import android.os.Bundle;
-import android.media.AudioManager;
-
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +33,8 @@ Button btn_play;
 Button btn_stop;
 
 MediaPlayer mediaPlayer;
+int voice_flag = 0;
+//ImageView imageView = findViewById(R.id.imageView);
 
 //    @Override
 //    protected void onDestroy(){
@@ -47,7 +49,8 @@ MediaPlayer mediaPlayer;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_kih);
-        
+
+        Button changeBtn = (Button) findViewById(R.id.button4);
         ListView listView = findViewById(R.id.listView);
         List<String> list = new ArrayList<>();
         list.add("그럴수도 있긴해");
@@ -55,6 +58,7 @@ MediaPlayer mediaPlayer;
         list.add("당했다 ..");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list){
+            @Override
             public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
                 TextView tv = (TextView)view.findViewById(android.R.id.text1);
@@ -65,28 +69,66 @@ MediaPlayer mediaPlayer;
         listView.setAdapter(adapter);
 
 
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                if(voice_flag == 0) {
                     if (position == 0) {
+                        voice_flag = 1;
                         mediaPlayer =
                                 MediaPlayer.create(MainActivity_kih.this, R.raw.kih_1);
                         mediaPlayer.start();
                         //그럴수도 있긴해
+                        sleep(4000);
+                        voice_flag = 0;
                     }
                     if (position == 1) {
+                        voice_flag = 1;
                         mediaPlayer = MediaPlayer.create(MainActivity_kih.this, R.raw.kih_2);
                         mediaPlayer.start();
                         //네 !~
+                        sleep(3000);
+                        voice_flag = 0;
                     }
                     if (position == 2) {
+                        voice_flag = 1;
                         mediaPlayer = MediaPlayer.create(MainActivity_kih.this, R.raw.kih_3);
                         mediaPlayer.start();
                         //아 협박까지 당했다 ..
+                        sleep(4000);
+                        voice_flag = 0;
                     }
                 }
-            });
+            }
 
+        });
+
+        changeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                intent.setAction(Intent.ACTION_PICK);
+                activityResultLauncher.launch(intent);
+
+            }
+        });
 
     }
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == RESULT_OK) {
+                        Intent intent = result.getData();
+                        Uri uri = intent.getData();
+                        ImageView imageView = findViewById(R.id.imageView);
+                        imageView.setImageURI(uri);
+                    }
+                }
+            }
+    );
+
 }
